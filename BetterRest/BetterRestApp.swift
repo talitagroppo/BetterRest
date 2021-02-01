fd76fc41096263a19382fb6a157b08f495b161c8
Talita Groppo <talitagroppo@icloud.com>
2021-02-01 15:50:19 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Talita Groppo on 29/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
